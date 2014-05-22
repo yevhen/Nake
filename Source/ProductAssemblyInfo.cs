e8fe2eb5fd76d6a36ce361196dcf79ad3e452b1a
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-05-22 18:54:55 +0300

    Version bump

--- Source/ProductAssemblyInfo.cs
+++ Source/ProductAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyInformationalVersion("2.0.1-beta")]
 [assembly: AssemblyCopyright("Copyright © Yevhen Bobrov 2013")]
