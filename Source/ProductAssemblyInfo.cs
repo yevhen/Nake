0e8d87f181bcb8f163a714797a13836cfbf6a379
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-05-28 21:54:24 +0300

    Unsigned Roslyn build

--- Source/ProductAssemblyInfo.cs
+++ Source/ProductAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.5.0")]
-[assembly: AssemblyInformationalVersion("2.0.5-beta")]
+[assembly: AssemblyVersion("2.0.6.0")]
+[assembly: AssemblyInformationalVersion("2.0.6-beta")]
 [assembly: AssemblyCopyright("Copyright © Yevhen Bobrov 2013")]
