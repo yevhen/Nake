5325bba76861db33be639c8a83f9adb23453df51
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-06-22 18:56:39 +0300

    Reverted back env var scoping

--- Source/Utility/Env.cs
+++ Source/Utility/Env.cs
@@ -12,59 +12,79 @@ namespace Nake
         /// <summary>
         /// An entry point for set of helper methods to deal with environment variables
         /// </summary>
-        public static readonly Scope Var = new Scope(EnvironmentVariableTarget.Process);
+        public static readonly Variables Var = new Variables();
 
         /// <summary>
-        /// Contains actual methods to deal with environment variables
+        /// An entry point for set of helper methods to deal with environment variables
         /// </summary>
-        public class Scope
+        public class Variables : EnvironmentScope
         {
-            readonly EnvironmentVariableTarget target;
-
-            internal Scope(EnvironmentVariableTarget target)
-            {
-                this.target = target;
-            }
+            /// <summary>
+            /// Allows to manipulate environment variables on the user-level
+            /// </summary>
+            public readonly EnvironmentScope User = new EnvironmentScope(EnvironmentVariableTarget.User);
 
             /// <summary>
-            /// Gets or sets the envrionment variable with the specified name.
+            /// Allows to manipulate environment variables on the machine-level
             /// </summary>
-            /// <value> The string value. </value>
-            /// <param name="name">The variable name.</param>
-            /// <returns>Value or <c>null</c> if environment variable does not exists</returns>
-            public string this[string name]
-            {
-                get
-                {
-                    return Defined(name)
-                            ? Environment.GetEnvironmentVariable(name, target)
-                            : null;
-                }
-                set
-                {
-                    Environment.SetEnvironmentVariable(name, value, target);
-                }
-            }
+            public readonly EnvironmentScope Machine = new EnvironmentScope(EnvironmentVariableTarget.Machine);
 
-            /// <summary>
-            /// Checks whether there is an environment variable with the specified name is defined (exists).
-            /// </summary>
-            /// <param name="name">The name of variable.</param>
-            /// <returns><c>true</c> if defined, <c>false</c> otherwise</returns>
-            public bool Defined(string name)
-            {
-                return Environment.GetEnvironmentVariable(name, target) != null;
-            }
+            internal Variables()
+                : base(EnvironmentVariableTarget.Process)
+            {}
+        }
+    }
 
-            /// <summary>
-            /// Returns all environment varaibles as name=value pairs
-            /// </summary>
-            /// <returns>Array of environment variable pairs</returns>
-            public string[] All()
+    /// <summary>
+    /// Contains actual methods to deal with environment variables
+    /// </summary>
+    public class EnvironmentScope
+    {
+        readonly EnvironmentVariableTarget target;
+
+        internal EnvironmentScope(EnvironmentVariableTarget target)
+        {
+            this.target = target;
+        }
+
+        /// <summary>
+        /// Gets or sets the envrionment variable with the specified name.
+        /// </summary>
+        /// <value> The string value. </value>
+        /// <param name="name">The variable name.</param>
+        /// <returns>Value or <c>null</c> if environment variable does not exists</returns>
+        public string this[string name]
+        {
+            get
             {
-                return (from DictionaryEntry entry in Environment.GetEnvironmentVariables(target)
-                        select entry.Key + "=" + entry.Value).ToArray();
+                return Defined(name)
+                           ? Environment.GetEnvironmentVariable(name, target)
+                           : null;
             }
+            set
+            {
+                Environment.SetEnvironmentVariable(name, value, target);
+            }
+        }
+
+        /// <summary>
+        /// Checks whether there is an environment variable with the specified name is defined (exists).
+        /// </summary>
+        /// <param name="name">The name of variable.</param>
+        /// <returns><c>true</c> if defined, <c>false</c> otherwise</returns>
+        public bool Defined(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, target) != null;
+        }
+
+        /// <summary>
+        /// Returns all environment varaibles as name=value pairs
+        /// </summary>
+        /// <returns>Array of environment variable pairs</returns>
+        public string[] All()
+        {
+            return (from DictionaryEntry entry in Environment.GetEnvironmentVariables(target)
+                    select entry.Key + "=" + entry.Value).ToArray();
         }
     }
 }
