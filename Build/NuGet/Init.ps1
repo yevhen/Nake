62483c54a1bf96efc678f3397ebe8e0107541851
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-07-12 17:28:08 +0300

    Removed leftovers from Nuget initialization script

--- Build/NuGet/Init.ps1
+++ Build/NuGet/Init.ps1
@@ -1,18 +1,8 @@
-﻿$scriptDir = split-path $script:MyInvocation.MyCommand.Path
-$nuget = "$scriptDir\NuGet.exe"
-$roslynPath = "$scriptDir\net45\Roslyn"
-
-if (Test-Path $roslynPath) {
-	Exit
-}
-
-Write-Host "Installing Roslyn CTP ..." -ForegroundColor DarkYellow
-&nuget install Roslyn.Compilers.CSharp -version 1.2.20906.2 -o $roslynPath
-
-$rootDir = (get-item $scriptDir ).parent.parent.parent.FullName
-$samplesDir = Join-Path (get-item $scriptDir ).parent.FullName "sample"
-$sampleCsxFile = "Nake.csx" 
-$sampleBatFile = "Nake.bat" 
+﻿$scriptDir      = split-path $script:MyInvocation.MyCommand.Path
+$rootDir        = (get-item $scriptDir ).parent.parent.parent.FullName
+$samplesDir     = Join-Path (get-item $scriptDir ).parent.FullName "sample"
+$sampleCsxFile  = "Nake.csx" 
+$sampleBatFile  = "Nake.bat" 
 
 if ((Test-Path "$rootDir\Nake.csx") -or (Test-Path "$rootDir\Nake.bat")) {
 	Exit
