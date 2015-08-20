3fefbea01c2541fcfa3463c2c9141ce7cf49333a
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-08-21 01:15:08 +0300

    Indicate pre-release version

--- Source/ProductAssemblyInfo.cs
+++ Source/ProductAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyInformationalVersion("2.4.0")]
+[assembly: AssemblyInformationalVersion("2.4.0-alpha")]
 [assembly: AssemblyCopyright("Copyright © Yevhen Bobrov 2013-2015")]
