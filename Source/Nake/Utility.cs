c6b32f9a9ebf04c6eed3ceefa21da8a085a62176
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2020-02-14 06:21:49 +0200

    Remove win32 code for determining if running in powershell

--- Source/Nake/Utility.cs
+++ Source/Nake/Utility.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace Nake
 {
@@ -20,112 +18,6 @@ namespace Nake
 
     static class Runner
     {
-        public static string Prefix = ParentProcess.IsPowerShell ? @".\" : "";
-
-        public static string Label(string runner = null)
-        {
-            return Prefix + (runner ?? "nake");
-        }
-    }
-
-    static class ParentProcess
-    {
-        public static bool IsPowerShell;
-
-        static ParentProcess()
-        {
-            var parent = GetParentProcess();
-
-            while (parent != null)
-            {
-                if (parent.ProcessName == "powershell")
-                {
-                    IsPowerShell = true;
-                    break;
-                }
-
-                parent = GetParentProcess(parent);
-            }
-        }
-
-        static Process GetParentProcess()
-        {
-            return GetParentProcess(Process.GetCurrentProcess());
-        }
-
-        static Process GetParentProcess(Process process)
-        {
-            var parentPid = 0;
-            var processPid = process.Id;
-
-            var oHnd = IntPtr.Zero;
-            try
-            {
-                oHnd = CreateToolhelp32Snapshot(Th32CsSnapprocess, 0);
-                if (oHnd == IntPtr.Zero)
-                    return null;
-
-                var procInfo = new PROCESSENTRY32 {dwSize = (uint) Marshal.SizeOf(typeof(PROCESSENTRY32))};
-                if (Process32First(oHnd, ref procInfo) == false)
-                    return null;
-
-                do
-                {
-                    if (processPid == procInfo.th32ProcessID)
-                        parentPid = (int) procInfo.th32ParentProcessID;
-
-                }
-                while (parentPid == 0 && Process32Next(oHnd, ref procInfo));
-
-                try
-                {
-                    return parentPid > 0 ? Process.GetProcessById(parentPid) : null;
-                }
-                catch (ArgumentException) 
-                {
-                    return null;
-                }
-                catch (InvalidOperationException)
-                {
-                    return null;
-                }
-            }
-            finally
-            {
-                if (oHnd != IntPtr.Zero)
-                    CloseHandle(oHnd);
-            }
-        }
-
-        const uint Th32CsSnapprocess = 2;
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr CreateToolhelp32Snapshot(uint dwFlags, uint th32ProcessId);
-
-        [DllImport("kernel32.dll")]
-        private static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
-
-        [DllImport("kernel32.dll")]
-        private static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct PROCESSENTRY32
-        {
-            public uint dwSize;
-            public readonly uint cntUsage;
-            public readonly uint th32ProcessID;
-            public readonly IntPtr th32DefaultHeapID;
-            public readonly uint th32ModuleID;
-            public readonly uint cntThreads;
-            public readonly uint th32ParentProcessID;
-            public readonly int pcPriClassBase;
-            public readonly uint dwFlags;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)] 
-            public readonly string szExeFile;
-        }
-
-        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool CloseHandle(IntPtr hObject);
+        public static string Label(string runner = null) => runner ?? "nake";
     }
 }
