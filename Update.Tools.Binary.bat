89b19fa2648c1c781b9f60c7e8c7276e7bb14621
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2020-04-02 20:42:59 +0300

    Cleanup

--- Update.Tools.Binary.bat
+++
@@ -1,3 +0,0 @@
-dotnet publish Source/Nake/Nake.csproj -c Release -o Tools/Nake
-dotnet publish Source/Meta/Meta.csproj -c Release -o Tools/Nake
-dotnet publish Source/Utility/Utility.csproj -c Release -o Tools/Nake
--- update.tools.binary.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dotnet publish ./Source/Nake/Nake.csproj -c Release -o ./Tools/Nake
