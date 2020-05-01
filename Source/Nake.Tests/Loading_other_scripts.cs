8e70f50d70efb179b28ae0950b0ddf7d12c6700f
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2020-05-01 18:05:20 +0300

    Add failing test for multi-file support

---
+++ Source/Nake.Tests/Loading_other_scripts.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace Nake
+{
+    [TestFixture]
+    class Loading_other_scripts : CodeFixture
+    {
+        [Test]
+        public void With_load_directive()
+        {
+            var importedScriptPath = TempFilePath();
+
+            BuildFile(importedScriptPath, @"                
+                [Nake] void Imported() 
+                { 
+                    var dir = ""%NakeScriptDirectory%"";                    
+                    Env.Var[""Imported_NakeScriptDirectory""] = dir;
+                }
+            ");
+
+            BuildFile($@"
+                #load ""{importedScriptPath}""
+                [Nake] void Default(){{}}
+            ");
+
+            Invoke("Imported");
+
+            Assert.That(Env.Var["Imported_NakeScriptDirectory"], Is.EqualTo(importedScriptPath.DirectoryName));
+        }
+    }
+}
